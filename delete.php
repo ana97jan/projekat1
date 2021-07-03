bec7d30b56171699ff71a520428b0fb3c84caa1a
Ana Janovic <janovicana97@gmail.com>
2021-07-03 23:42:23 +0200

    del

--- delete.php
+++ delete.php
@@ -2,10 +2,7 @@
     $naocareID = $_GET['naocareID'];
     $url = 'http://localhost/projekat/naocare/'. $naocareID;
     $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json','Content-Type: application/json'));
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-
+   
     $curl_odgovor = curl_exec($curl);
     curl_close($curl);
     $json_objekat = json_decode($curl_odgovor);
